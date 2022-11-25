aa1414b2946e4c32b3ba61cbdc8de87a3034b7bd
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-25 14:47:16 +0600

    added services in specific id

--- index.js
+++ index.js
@@ -29,13 +29,13 @@ async function run() {
             res.send(services);
         });
 
-        // //todo get specific service
-        // app.get('/services/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const query = { _id: ObjectId(id) };
-        //     const service = await serviceCollection.findOne(query);
-        //     res.send(service);
-        // });
+        //todo get specific service
+        app.get('/services/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const service = await serviceCollection.findOne(query);
+            res.send(service);
+        });
 
 
     }
