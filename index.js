ff474bb4a0f8d61bdf1ad228e7254492e1bf145d
Abu Sayeed <sayeed@pop-os.localdomain>
2022-11-27 19:37:02 +0600

    added update user route

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ async function run() {
         })
 
         //todo get all the users data
-        app.get('/users', async(req, res) =>{
+        app.get('/users', async (req, res) => {
             const query = {};
             const users = await usersCollection.find(query).toArray();
             res.send(users);
@@ -111,6 +111,21 @@ async function run() {
             const result = await usersCollection.insertOne(user);
             res.send(result);
         });
+
+        // todo update the user api
+        app.put('/users/admin/:id', async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const options = { upsert: true };
+            const updatedDoc = {
+                $set: {
+                    role: 'admin'
+                }
+            }
+            const result = await usersCollection.updateOne(filter, updatedDoc, options);
+            res.send(result);
+        });
+
 
     }
     finally {
